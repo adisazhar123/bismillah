f435a829ad298aa2d868615b7b8b47789c5ee7e6
adisazhar123 <adisazhar123@gmail.com>
2018-03-02 01:28:11 +0700

    changed text btn

--- application/views/peserta/welcome_peserta.php
+++ application/views/peserta/welcome_peserta.php
@@ -7,7 +7,7 @@
             <h3>selamat datang tim ___ pada schematics 2018</h3>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               <br> <br>
-              <button type="button" class="btn btn-primary show-petunjuk" name="button">Mulai Mengerjakan</button>
+              <button type="button" class="btn btn-warning show-petunjuk" name="button">Lihat Petunjuk</button>
           </div>
         </div>
       </div>
